369cb03a94e20e9781c5c1cabecbf70ae0da61e5
Aleksandr Blyudenov <abludenov@gmail.com>
2023-05-02 07:33:21 +0300

    Add BaseEntity, NamedEntity, application.yaml Fix pom.xml

---
+++ src/main/java/com/abdddev/restaurantvoting/HasIdAndEmail.java
@@ -0,0 +1,5 @@
+package com.abdddev.restaurantvoting;
+
+public interface HasIdAndEmail extends HasId {
+    String getEmail();
+}
--- src/main/java/com/abdddev/restaurantvoting/RestaurantVotingApplication.java
+++ src/main/java/com/abdddev/restaurantvoting/RestaurantVotingApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestaurantVotingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantVotingApplication.class, args);
 	}
-
 }
